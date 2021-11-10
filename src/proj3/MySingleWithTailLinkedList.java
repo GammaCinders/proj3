8d43e527692b2fe0261b1b46d0f46d25c9abfa73
Keagen <keagen@keag.com>
2021-11-10 17:06:17 -0500

    I added some things

--- src/proj3/MySingleWithTailLinkedList.java
+++ src/proj3/MySingleWithTailLinkedList.java
@@ -57,46 +57,85 @@ public class MySingleWithTailLinkedList implements Serializable
             tail = top = new Node(rental, null);
             return;
         } else {
-		tail.setNext(new Node(rental, null));
-		tail = tail.getNext();
-	}
+		    tail.setNext(new Node(rental, null));
+		    tail = tail.getNext();
+    	}
 	
     }
 
-    public Rental remove(int index) {
-        //  more code goes here.
 
-    	
-			    
-	    if(top == null || index >= size) {
-		throw new IllegalArgumentException();
+    public Rental remove(int index) {
+        Rental removedRental;
+
+        //Illegal Arg case
+	    if(size() == 0 || index >= size()) {
+		    throw new IllegalArgumentException();
 	    }
+
+        //Case for just top
+        if(size() == 1) {
+            removedRental = top.getData();
+            top = tail = null;
+            return removedRental;
+        }
+
+        //Case removing top
+        if(index == 0) {
+            removedRental = top.getData();
+            top = top.getNext();
+            return removedRental;
+        }
+
+        //Case removing tail
+        if(index == size()-1) {
+            //have to get node before tail to remove tail
+            Node temp = top;
+            while(temp.getNext() != tail) {
+                temp = temp.getNext();
+            }
+
+            removedRental = temp.getNext().getData();
+            temp.setNext(null);
+            tail = temp;
+
+            return removedRental;
+        }
+
+        //Case general middle removal
+        Node temp = top;
+        //gets the node before the desired node
+        for(int i=0; i<index-1; i++) {
+            temp = temp.getNext();
+        }
+
+        removedRental = temp.getNext().getData();
+        temp.setNext(temp.getNext().getNext());
+
+        return removedRental;
     }
 
     public Rental get(int index) {
-        //  more code goes here.
+	    //Not sure if this is the best way,
+	    //if index is invalid, this will still return
 
-	//Not sure if this is the best way,
-	//if index is invalid, this will still return
-	//null if top is null
+	    //null if top is null
         if (top == null) {
             return null;
-	}
+	    }
 
-	//Index error checks
-	if(index >= size) {
-	    throw new IllegalArgumentException();
-	} else {
-		//code to find rental at index
-		Node temp = top;
-		while(index > 0) {
-			temp = temp.getNext();
-			index--;
-		}
-		
-		return temp.getData();
+	    //Index error checks
+	    if(index >= size()) {
+	        throw new IllegalArgumentException();
+	    } else {
+		    //code to find rental at index
+            Node temp = top;
+            while(index > 0) {
+			    temp = temp.getNext();
+			    index--;
+		    }
 
-	}	
+            return temp.getData();
+	    }
 	
     }
 
